1d041f278b6d419c3fb426cb271eee73266795b5
johninsan <johninsan@gmail.com>
2022-08-01 10:57:28 +0700

    ganti route name vendor

--- resources/views/mitra/create.blade.php
+++ resources/views/mitra/create.blade.php
@@ -90,7 +90,7 @@
                             </div>
                             <div class="text-center">
                                 <button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
-                                <a href="{{ route('vendor.index') }}" type="button"
+                                <a href="{{ route('vendor_data.index') }}" type="button"
                                     class="btn btn-info mt-4">{{ __('Back') }}</a>
                             </div>
                     </div>
