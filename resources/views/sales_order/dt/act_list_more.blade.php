271ba6aee59f5c43b942ed4ca2e3fe6f3c29f082
johninsan <johninsan@gmail.com>
2022-08-05 16:32:02 +0700

    tambah profit sementara

--- resources/views/sales_order/dt/act_list_more.blade.php
+++ resources/views/sales_order/dt/act_list_more.blade.php
@@ -7,6 +7,8 @@
             data-target="#sales_selling">Selling </a>
         <a href="#" data-id="{{ $data['id'] }}" class="dropdown-item infoB" data-toggle="modal"
             data-target="#sales_buying">Buying </a>
+        <a href="#" data-id="{{ $data['id'] }}" class="dropdown-item infoP" data-toggle="modal"
+            data-target="#sales_profit">Profit </a>
         <a href="#" data-id="{{ $data['id'] }}" class="dropdown-item infoDP" data-toggle="modal"
             data-target="#sales_dp">Down Payment </a>
     </div>
