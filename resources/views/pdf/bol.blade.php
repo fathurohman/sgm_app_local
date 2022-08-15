dbfdb43ba91928bcf125d9d119f67cb3a61e9e88
johninsan <johninsan@gmail.com>
2022-08-15 11:09:51 +0700

    fix tampilan pdf bol

--- resources/views/pdf/bol.blade.php
+++ resources/views/pdf/bol.blade.php
@@ -13,7 +13,8 @@
     <div class="table-page">
         <table style="table-layout:fixed;">
             <tr>
-                <th height="30" rowspan="2" style="width: 55%;vertical-align: top;text-align:left">
+                <th height="30" rowspan="2"
+                    style="width: 55%;vertical-align: top;text-align:left;padding-right:5px">
                     {{ $data->Shipper }}</th>
                 <th style="width: 40%; text-align:center">{{ $data->BL_NO }}</th>
             </tr>
@@ -26,19 +27,13 @@
     <div class="table-consignee">
         <table style="table-layout: fixed">
             <tr>
-                <th style="width:55%; text-align:left"></th>
-                <th style="width:45%; text-align:left"></th>
-            </tr>
-            <tr>
-                <th height="50" rowspan="5" style="width:55%; vertical-align: top; text-align:left">
+                <th height="50" rowspan="2"
+                    style="width:55%; vertical-align: top; text-align:left;padding-right:10px">
                     {{ $data->Consignee }}</th>
-                <th height="50" rowspan="4" style="width:45%; vertical-align: top; text-align:left">
+                <th height="45" style="width:45%; vertical-align: top; text-align:left">
                     a{{ $data->forwarding_agent }}
                 </th>
             </tr>
-            <tr></tr>
-            <tr></tr>
-            <tr></tr>
             <tr>
                 <th style="width:45%; text-align:center">a{{ $data->Point_Country_Origin }}</th>
             </tr>
@@ -47,7 +42,9 @@
     <div class="table-notify">
         <table style="table-layout: fixed">
             <tr>
-                <th height="60" style="width:55%; vertical-align: top; text-align:left">{{ $data->Notify_Party }}
+                <th height="60"
+                    style="width:55%; vertical-align: top; text-align:left;padding-bottom:5px;padding-right:10px">
+                    {{ $data->Notify_Party }}
                 </th>
                 <th rowspan="3" style="width:45%; vertical-align: top; text-align:left">{{ $data->Obtain_Delivery }}
                 </th>
@@ -69,6 +66,8 @@
                 </table>
             </tr>
         </table>
+    </div>
+    <div class="table-discharge">
         <table style="table-layout: fixed">
             <tr>
                 <th style="width: 25%;text-align:center">a {{ $data->Port_Dischage }}</th>
@@ -88,10 +87,10 @@
                 <th style="width: 10%;text-align:left"></th>
             </tr>
             <tr>
-                <td height="180">{{ $data->Marks_Number }}</td>
+                <td class="marks-td" height="180">{{ $data->Marks_Number }}</td>
                 <td height="180">{{ $data->No_Cont_Pkgs }}</td>
-                <td height="180">{{ $data->Description_Packages_Goods }}</td>
-                <td height="180">{{ $data->Gross_Weight }}</td>
+                <td class="marks-td" height="180">{{ $data->Description_Packages_Goods }}</td>
+                <td class="marks-td" height="180">{{ $data->Gross_Weight }}</td>
                 <td height="180">{{ $data->Measurement }}</td>
             </tr>
         </table>
@@ -105,13 +104,13 @@
                 <th style="width: 60%;"></th>
             </tr>
             <tr>
-                <td height="100" style="text-align:left">a{{ $data->Freight_Charges }}</td>
-                <td height="100" style="text-align:left">a{{ $data->Prepaid }}</td>
-                <td height="100" style="text-align:left">a{{ $data->Collect }}</td>
-                <td height="100"></td>
+                <td height="75" style="text-align:left">a{{ $data->Freight_Charges }}</td>
+                <td height="75" style="text-align:left">a{{ $data->Prepaid }}</td>
+                <td height="75" style="text-align:left">a{{ $data->Collect }}</td>
+                <td height="75"></td>
             </tr>
             <tr>
-                <td style="text-align:center" colspan="3">{{ $data->Total_Charges }}</td>
+                <td style="text-align:center;padding-bottom:10px" colspan="3">{{ $data->Total_Charges }}</td>
                 <td style="text-align: right">{{ $data->BY }}</td>
             </tr>
             <tr>
@@ -119,6 +118,8 @@
                 <td></td>
             </tr>
         </table>
+    </div>
+    <div class="table-original">
         <table>
             <th style="width: 37,5%; text-align:center">{{ $data->No_Original }}</th>
             <th style="width: 37,5%; text-align:center">{{ $data->Place_Date_Issue }}</th>
